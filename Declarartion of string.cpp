f7b07be41225472a4d5519db2f0b05e1ec23ea5d
theadityabasak <orandom46@gmail.com>
2024-08-28 09:50:01 +0530

    Add files via upload
    Program to declare and initialise a string

---
+++ Declarartion of string.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{   // Declaration and initialization of 1st string
+    string str1 = "HELLO AND WELCOME TO SIT";
+    // Declarartion and initialization of 2nd string
+    string str2 = "MY NAME IS ADITYA";
+    cout<<str1<<endl<<str2;
+}
+
+
+//HELLO AND WELCOME TO SIT
+//MY NAME IS ADITYA
+
